11f5046dfec07f08a5b2e3c91e2ed38c693a5d67
Charlie <porthose.cjsmo@gmail.com>
2020-12-09 22:47:30 -0800

    fix upload

--- pi_survel.py
+++ pi_survel.py
@@ -104,12 +104,13 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 					pcm = PiCamMail()
 					print(t.path)
 					cv2.imwrite(t.path, frame)
+					pcm.charlie(t.path)
 					# upload the image to Dropbox and cleanup the tempory image
 					print("[UPLOAD] {}".format(ts))
 					path = "/{base_path}/{timestamp}.jpg".format(
 					    base_path=conf["dropbox_base_path"], timestamp=ts)
 					client.files_upload(open(t.path, "rb").read(), path)
-					pcm.charlie(t.path)
+					
 					# pcm.teresa(t.path)
 					# pcm.dylan(t.path)
 					t.cleanup()
